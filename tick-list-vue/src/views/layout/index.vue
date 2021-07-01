1dfc968f644a4e44b8bf4a236f4dc9111facb727
milk-abc <3292944779@qq.com>
2021-07-01 23:41:46 +0800

    feat:将Navbar加入layout中并调整样式

--- tick-list-vue/src/views/layout/index.vue
+++ tick-list-vue/src/views/layout/index.vue
@@ -1,17 +1,23 @@
 <!--  -->
 <template>
-  <div class="app-wrapper">
+  <div :class="classObj"
+       class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened"
+         class="drawer-bg"
+         @click="handleClickOutside" />
+    <!-- <sidebar class="sidebar-container" /> -->
     <div class="main-container">
-      <div>
-        <Navbar></Navbar>
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
       </div>
-      <AppMain></AppMain>
+      <app-main />
     </div>
   </div>
 </template>
 
 <script>
 import { Navbar, Sidebar, AppMain } from './components'
+import ResizeMixin from './mixin/ResizeHandler'
 export default {
   name: 'layout',
   components: {
@@ -19,18 +25,36 @@ export default {
     Sidebar,
     AppMain
   },
+  mixins: [ResizeMixin],
   data () {
     //这里存放数据
     return {
 
     };
   },
-  methods: {
-
+  computed: {
+    sidebar () {
+      return this.$store.state.app.sidebar
+    },
+    device () {
+      return this.$store.state.app.device
+    },
+    fixedHeader () {
+      return this.$store.state.settings.fixedHeader
+    },
+    classObj () {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile'
+      }
+    }
   },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created () {
-
+  methods: {
+    handleClickOutside () {
+      this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+    }
   }
 }
 </script>
@@ -42,7 +66,21 @@ export default {
   position: relative;
   height: 100%;
   width: 100%;
+  &.mobile.openSidebar {
+    position: fixed;
+    top: 0;
+  }
 }
+.drawer-bg {
+  background: #000;
+  opacity: 0.3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
+}
+
 .fixed-header {
   position: fixed;
   top: 0;
@@ -50,5 +88,13 @@ export default {
   z-index: 9;
   width: calc(100% - #{$sideBarWidth});
   transition: width 0.28s;
+}
+
+.hideSidebar .fixed-header {
+  width: calc(100% - 54px);
+}
+
+.mobile .fixed-header {
+  width: 100%;
 }
 </style>
