ec782af86e5a6cb99db9b39bd98203b1576c3593
milk-abc <3292944779@qq.com>
2021-07-04 14:58:52 +0800

    替换为云服务器地址

--- tick-list-vue/src/axios.js
+++ tick-list-vue/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Element from "element-ui";
 import store from "./store";
 import router from "./router";
-axios.defaults.baseURL = "http://localhost:8888";
+axios.defaults.baseURL = "http://120.27.240.131:8888";
 axios.interceptors.request.use(config => {
   console.log("前置拦截");
   // 可以统一设置请求头
