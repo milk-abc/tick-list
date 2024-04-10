d7bab741f98233bca9d94801d6043ea7cf2e68a7
milk-abc <3292944779@qq.com>
2024-04-11 00:44:46 +0800

    fix(type): svg

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 const captcha = require("../utils/Code");
 const { setString } = require("../utils/redis-code.js");
-// const {
-//   verifyUsers,
-//   handlePassword,
-// } = require("../middleware/user.middleware.js");
-// const UserController = require("../controller/user.controller.js");
-// const { PUBLIC_KEY } = require("../app/config.js");
+const {
+  verifyUsers,
+  handlePassword,
+} = require("../middleware/user.middleware.js");
+const UserController = require("../controller/user.controller.js");
+const { PUBLIC_KEY } = require("../app/config.js");
 var router = express.Router();
 
 /**
@@ -15,15 +15,15 @@ var router = express.Router();
  * 数据有效后，对密码进行md5加密，放入数据库中创建用户
  */
 // router.post("/register", [verifyUsers, handlePassword, UserController.create]);
-// router.get("/getPublicKey", function (req, res, next) {
-//   /**
-//    * 用户登录
-//    * 取出表单数据查询数据库中是否有该用户，密码是否正确
-//    * 如果正确的话，将token传给客户端
-//    */
-//   console.log("hhhhhhh");
-//   res.status(200).send({ code: 200 });
-// });
+router.get("/getPublicKey", function (req, res, next) {
+  /**
+   * 用户登录
+   * 取出表单数据查询数据库中是否有该用户，密码是否正确
+   * 如果正确的话，将token传给客户端
+   */
+  console.log("hhhhhhh");
+  res.status(200).send({ data: PUBLIC_KEY, code: 200 });
+});
 // /* GET home page. */
 router.get("/login", function (req, res, next) {
   /**
@@ -46,7 +46,7 @@ router.get("/getSecurityCode", function (req, res, next) {
   console.log("code");
   let code = captcha();
   setString(req.sessionID, code.text.toLowerCase(), 300);
-  res.type("svg");
+  res.type("html"); //res.type('svg'); // 使用ejs等模板时报错
   res.status(200).send({ data: code.data, code: 200 });
   res.end();
 });
