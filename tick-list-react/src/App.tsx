e167d6ad0ac2e936b53443b34c535c6143199d94
milk-abc <3292944779@qq.com>
2024-05-09 12:26:50 +0800

    fix(demo): demo

--- tick-list-react/src/App.tsx
+++ tick-list-react/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routers from "@/router";
-import Login from "./components/login";
 function App() {
   return (
     <BrowserRouter>
--- tick-list-react/src/components/login/index.tsx
+++ tick-list-react/src/components/login/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import type { FormProps } from "antd";
 import { Button, Checkbox, Form, Input } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "./index.less";
 
 type FieldType = {
   username?: string;
@@ -16,47 +20,70 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
 
-const Login: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: "Please input your username!" }]}
-    >
-      <Input />
-    </Form.Item>
+const Login: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: "Please input your password!" }]}
-    >
-      <Input.Password />
-    </Form.Item>
+  return (
+    <div className="loginContainer">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          className="username"
+          name="username"
+          rules={[{ required: true, message: "请输入用户名!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="用户名"
+          />
+        </Form.Item>
+        <Form.Item
+          className="password"
+          name="password"
+          rules={[{ required: true, message: "请输入密码!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
+        <Form.Item
+          className="confirmCode"
+          name="confirmCode"
+          rules={[{ required: true, message: "请输入验证码!" }]}
+        >
+          <Input placeholder="验证码" />
+        </Form.Item>
+        <Form.Item>
+          <div className="userAction">
+            <a className="login-form-register" href="">
+              注册
+            </a>
+            <a className="login-form-forgot" href="">
+              忘记密码
+            </a>
+          </div>
+        </Form.Item>
 
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
